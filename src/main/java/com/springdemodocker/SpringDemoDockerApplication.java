8426b7284f286e8efc9aed504502dfc180601050
Prasanta1329 <pande.prasant1997@gmail.com>
2024-03-13 18:19:07 +0530

    first commit

---
+++ src/main/java/com/springdemodocker/SpringDemoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.springdemodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoDockerApplication.class, args);
+	}
+
+}
